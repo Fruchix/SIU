0a1a9b30e87ff8b3325ba2b3f6f2a03a9b734aee
Fruchix <fruchix@gmail.com>
2024-09-16 23:20:19 +0200

    refactor: rename functions + all deps checks at the same time
    All deps are now checked, and installed only when all are finished.

--- install_script.sh
+++ install_script.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# stop at first error/exit
-# set -e
-
 SIU_DIR=$HOME/.siu
 DEPS_DIR=$SIU_DIR/deps
 
-INCLUDE_NCURSES=/usr/include
+missing_dependencies=()
 
-check_return_code()
+check::return_code()
 {
     if [[ $? -ne 0 ]]; then
         if [[ $# -gt 0 ]]; then
@@ -22,87 +19,99 @@ check_return_code()
     fi
 }
 
-check_ncurses_installed()
+check::zsh_depency::ncurses()
 {
     # Check if ncurses is installed at:
     # /usr/include/ncurses/ncurses.h
     # /usr/include/ncursesw/ncurses.h
-    echo "checking for ncurses... "
-    if [[ -f $INCLUDE_NCURSES/ncurses/ncurses.h && -f $INCLUDE_NCURSES/ncursesw/ncurses.h ]]; then
-        echo -n "ok"
+    echo -n "checking for ncurses... "
+    if [[ -f /usr/include/ncurses/ncurses.h || -f /usr/include/ncursesw/ncurses.h ]]; then
+        echo "ok"
+        true
         return
     fi
 
     echo "no"
+    missing_dependencies=($missing_dependencies "ncurses")
+    false
+}
+
+check::zsh_depency::make()
+{
+    echo -n "checking for make... "
+    make --version 2&>1 /dev/null
+    check::return_code "make is not installed. Stopping installation."
+    echo "ok"
+    true
+}
+
+install::ncurses()
+{
     echo "Installing ncurses from source."
-    install_ncurses
-    INCLUDE_NCURSES=$DEPS_DIR/include
-}
 
-check_make_installed()
-{
-    echo "checking for make... "
-    make --version
-    check_return_code "make is not installed. Stopping installation."
-    echo -n "ok"
-}
-
-install_ncurses()
-{
     mkdir -p $DEPS_DIR
     pushd $DEPS_DIR
 
     # get ncurses archive
     wget https://invisible-island.net/archives/ncurses/ncurses.tar.gz
-    check_return_code "ncurses install: could not download archive. Stopping installation."
+    check::return_code "ncurses install: could not download archive. Stopping installation."
 
     mkdir ncurses
-    check_return_code
+    check::return_code
     tar -xvf ncurses.tar.gz -C ncurses --strip-components 1
-    check_return_code "ncurses install: could not untar archive. Stopping installation."
+    check::return_code "ncurses install: could not untar archive. Stopping installation."
 
     cd ncurses
     ./configure --prefix=$DEPS_DIR --with-shared --enable-widec
-    check_return_code "zsh install: \"./configure\" did not work. Stopping installation."
+    check::return_code "ncurses install: \"./configure\" did not work. Stopping installation."
 
     make -j8
-    check_return_code "zsh install: \"make\" did not work. Stopping installation."
+    check::return_code "ncurses install: \"make\" did not work. Stopping installation."
 
     make install
-    check_return_code "zsh install: \"make install\" did not work. Stopping installation."
+    check::return_code "ncurses install: \"make install\" did not work. Stopping installation."
     popd
+
+    true
 }
 
-install_zsh()
+install::zsh()
 {
     mkdir -p $SIU_DIR
     cd $SIU_DIR
 
     # checking dependencies
-    check_make_installed
-    check_ncurses_installed
+    check::zsh_depency::make
+    check::zsh_depency::ncurses
+
+    # install all missing dependencies
+    for depency in "${missing_dependencies[@]}"; do
+        "install::$depency"
+        check::return_code "An unexpected error happened during the installation of dependency: $dependency"
+    done
 
     # download archive, should create an archive named zsh.tar.xz
     if [[ 1 -eq 1 ]]; then
         wget -O zsh.tar.xz https://sourceforge.net/projects/zsh/files/latest/download
-        check_return_code "zsh install: could not download archive. Stopping installation."
+        check::return_code "zsh install: could not download archive. Stopping installation."
     fi
     mkdir zsh
-    check_return_code
+    check::return_code
     tar -xvf zsh.tar.xz -C zsh --strip-components 1
-    check_return_code "zsh install: could not untar archive. Stopping installation."
+    check::return_code "zsh install: could not untar archive. Stopping installation."
 
     cd zsh
 
     ./configure --prefix=$SIU_DIR CPPFLAGS=-I$DEPS_DIR/include LDFLAGS=-L$DEPS_DIR/lib
-    check_return_code "zsh install: \"./configure\" did not work. Stopping installation."
+    check::return_code "zsh install: \"./configure\" did not work. Stopping installation."
 
     make -j8
-    check_return_code "zsh install: \"make\" did not work. Stopping installation."
+    check::return_code "zsh install: \"make\" did not work. Stopping installation."
 
     make install
-    check_return_code "zsh install: \"make install\" did not work. Stopping installation."
+    check::return_code "zsh install: \"make install\" did not work. Stopping installation."
+
+    true
 }
 
-
-install_zsh
+install::zsh
