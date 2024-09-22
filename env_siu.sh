a7e7b39c65c3352391e107410c5d7d7f3e63a919
Fruchix <fruchix@gmail.com>
2024-09-22 19:07:22 +0200

    feat: add creation of files siu_exports, siu_bashrc and siu_zshrc
    Now ~/.bashrc and ~/.zshrc source siu_exports, and respectively siu_bashrc and siu_zshrc.
    Add bsdmainutils in Dockerfile, needed by star (provides column utility).

--- env_siu.sh
+++ env_siu.sh
@@ -2,4 +2,6 @@
 
 SIU_DIR=$HOME/.siu
 SIU_DEPS_DIR=$SIU_DIR/deps
+SIU_EXPORTS=$SIU_DIR/siu_exports
 SIU_ZSHRC=$SIU_DIR/siu_zshrc
+SIU_BASHRC=$SIU_DIR/siu_bashrc
--- setup_siu.sh
+++ setup_siu.sh
@@ -5,25 +5,54 @@ source env_siu.sh
 init::siu_dirs()
 {
     mkdir -p $SIU_DIR
+    mkdir -p $SIU_DIR/bin
     mkdir -p $SIU_DEPS_DIR
+}
+
+init::siu_exports()
+{
+    echo "export SIU_DIR=${SIU_DIR}" >> "${SIU_EXPORTS}"
+    cat << "EOF" >> "${SIU_EXPORTS}"
+export SIU_EXPORTS=$SIU_DIR/siu_exports
+export SIU_ZSHRC=$SIU_DIR/siu_zshrc
+export SIU_BASHRC=$SIU_DIR/siu_bashrc
+
+export PATH=$PATH:$SIU_DIR/bin:$SIU_DEPS_DIR/bin
+EOF
+}
+
+init::siu_rc_files()
+{
+    touch $SIU_BASHRC
+    touch $SIU_ZSHRC
+    touch $SIU_EXPORTS
 }
 
 init::siu_zshrc()
 {
-    echo "export SIU_DIR=${SIU_DIR}" >> "${SIU_ZSHRC}"
-    cat << "EOF" >> "${SIU_ZSHRC}"
-export SIU_ZSHRC=$SIU_DIR/siu_zshrc
-
-export PATH=$PATH:$SIU_DIR/bin
+    cat << EOF >> $SIU_ZSHRC
+# Activate bash completion compatibility
+autoload -U +X bashcompinit
+bashcompinit
+autoload -U +X compinit
+compinit
 EOF
 }
 
 init::siu()
 {
-    echo "source $SIU_ZSHRC" >> ~/.zshrc
-    echo "export SIU_DIR=$SIU_DIR" >> ~/.bashrc
-    echo 'export PATH=$PATH:$SIU_DIR/bin' >> ~/.bashrc
+    echo -e "\n### Automaticaly added by SIU::init::siu ###" >> ~/.zshrc
+    echo "source $SIU_EXPORTS                      ### SIU::init::siu" >> ~/.zshrc
+    echo 'source $SIU_ZSHRC                        ### SIU::init::siu' >> ~/.zshrc
+    echo "### Automaticaly added by SIU::init::siu ###" >> ~/.zshrc
+
+    echo -e "\n### Automaticaly added by SIU::init::siu ###" >> ~/.bashrc
+    echo "source $SIU_EXPORTS                      ### SIU::init::siu" >> ~/.bashrc
+    echo 'source $SIU_BASHRC                       ### SIU::init::siu' >> ~/.bashrc
+    echo "### Automaticaly added by SIU::init::siu ###" >> ~/.bashrc
 
     init::siu_dirs
+    init::siu_rc_files
+    init::siu_exports
     init::siu_zshrc
 }
