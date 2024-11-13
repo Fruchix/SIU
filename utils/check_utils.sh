117193b9e20b7aae36a5c2c7420237a86f6a990a
Fruchix <fruchix@gmail.com>
2024-11-13 08:40:46 +0100

    feat: add check for tools dependencies

--- utils/check_utils.sh
+++ utils/check_utils.sh
@@ -42,6 +42,85 @@ function _siu::check::command_exists()
     command -v "$1" >/dev/null 2>&1
 }
 
+# _siu::check::tools_dependencies_worker <dep>
+#   Recursive worker for "_siu::check::tools_dependencies".
+#   Check if the provided dependency/tool is installed, and check its own dependencies recursively if not.
+#   If a missing dependency can't be installed (an external dependency), exit the program.
+# 
+#   Arguments:
+#       $1: name of the dependency to check
+#   Returns:
+#       None. The "tools" array now contains every dependency that needs to be installed before any the provided dependency.
+function _siu::check::tools_dependencies_worker()
+{
+    if [[ "$#" -lt 1 ]]; then
+        _siu::log::warning "Missing argument: <dep> (dependency to check)."
+        return
+    fi
+
+    local var_name list_deps
+
+    # checking each dependency that should already be installed on the system:
+    # if a dependency is missing then stop the installation
+    var_name="deps_${1}_external_"
+    if [[ -n "${!var_name}" ]]; then
+        read -a list_deps <<< "${!var_name}"
+        for dep in "${list_deps[@]}"; do
+            _siu::check::command_exists "${!dep}"
+            _siu::check::return_code "Checking for external dependency \"${!dep}\": not installed. Stopping installation." "Checking for external dependency \"${!dep}\": ok."
+        done
+    fi
+
+    # checking each dependency that can be installed by our scripts:
+    # if a dependency is missing then add the dependency in the list of tools to install (before the dependant tool)
+    var_name="deps_${1}_managed_"
+    if [[ -n "${!var_name}" ]]; then
+        read -a list_deps <<< "${!var_name}"
+        for dep in "${list_deps[@]}"; do
+            # if this dependency is already in the list of tools to install, skip checks
+            if [[ "${tools[*]}" =~ ${!dep} ]];then
+                continue
+            fi
+            #TODO: replace _siu::check::command_exists by a _siu::check_installed::<tool> command
+            _siu::check::command_exists "${!dep}"
+            _siu::check::return_code "Checking for managed dependency \"${!dep}\": not installed. Adding \"${!dep}\" to the list of tools to install." "Checking for managed dependency \"${!dep}\": ok." --no-exit
+            # if the dependency is missing 
+            # then check its own dependencies recursively
+            if [[ "$?" -ne 0 ]]; then
+                _siu::check::tools_dependencies_worker "${!dep}"
+            fi
+        done
+    fi
+
+    if [[ ! "${tools[*]}" =~ ${1} ]];then
+        tools=("${tools[@]}" "${1}")
+    fi
+}
+
+# _siu::check::tools_dependencies
+#   Check the dependencies for all tools contained in the "tools" array, recursively.
+#   Add the missing dependencies to the "tools" array, before there dependant tool.
+#   If a missing dependency can't be installed (an external dependency), exit the program.
+# 
+#   Example: this function will transform "tools=(fzf omz)" into "tools=(fzf ncurses zsh omz)" if zsh and ncurses are not installed
+# 
+#   Returns:
+#       None. The "tools" array now contains every dependency that needs to be installed before each tool.
+function _siu::check::tools_dependencies()
+{
+    local tmp_tools
+    
+    eval $(parse_yaml deps/dependencies.yaml "deps_")
+    eval $(parse_yaml tools/dependencies.yaml "deps_")
+
+    tmp_tools=("${tools[@]}")
+    tools=()
+    for t in "${tmp_tools[@]}"; do
+        # recursive worker
+        _siu::check::tools_dependencies_worker "${t}"
+    done
+}
+
 # _siu::check::dependency::ncurses
 #   Check if ncurses is installed, required by zsh.
 #   Check whether one of these two header files exists:
