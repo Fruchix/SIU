9fe4236b76f5b8c32d4209c173a79afa738b682b
Fruchix <fruchix@gmail.com>
2024-11-23 18:35:16 +0100

    feat: add function to compare two versions

--- utils/tool_versioning.sh
+++ utils/tool_versioning.sh
@@ -12,7 +12,7 @@ declare -A tools_installed
 # Outputs:
 #   Writes tool and version to the file SIU_TOOL_VERSIONS
 #######################################
-function _siu::versioning::create() {
+function _siu::versioning::create_file() {
     if [[ ! -f ${SIU_TOOL_VERSIONS} ]]; then
         touch "${SIU_TOOL_VERSIONS}"
     fi
@@ -62,9 +62,7 @@ function _siu::versioning::set_tool_version() {
         return 1
     fi
 
-    if [[ ! -f ${SIU_TOOL_VERSIONS} ]]; then
-        touch "${SIU_TOOL_VERSIONS}"
-    fi
+    _siu::versioning::create_file
 
     if ! grep -E "^$1=.*" "${SIU_TOOL_VERSIONS}" > /dev/null; then
 
@@ -102,4 +100,56 @@ function _siu::versioning::delete_tool() {
         _siu::log::error "Could not delete tool \"$1\" from versioning."
         return 1
     } && _siu::log::info "Successfully deleted tool \"$1\" from versioning."
+}
+
+#######################################
+# Compare two versions using format X.X.X
+# Arguments:
+#   <version1> first version to compare
+#   <operator> the operator to use, in [-eq,-ne,-ge,-gt,-le,-lt]
+#   <version2> second version to compare
+# Returns:
+#   0 if <version1> <operator> <version2>,
+#   1 if not,
+#   2 if there is not enough parameters
+#   3 if a parameter format is wrong
+#######################################
+function _siu::versioning::compare_versions() {
+    if [[ "$#" -lt 3 ]]; then
+        return 2
+    fi
+    local version1 operator version2 op1 op2 same_versions
+    version1=$1
+    operator=$2
+    version2=$3
+    if [[ ! "${version1}" =~ ^[0-9]+[.][0-9]+[.][0-9]+$ ]]; then
+        _siu::log::error "Bad parameter format: version1=\"${version1}\""
+        return 3
+    fi
+    if [[ ! "${version2}" =~ ^[0-9]+[.][0-9]+[.][0-9]+$ ]]; then
+        _siu::log::error "Bad parameter format: version2=\"${version2}\""
+        return 3
+    fi
+
+    same_versions=$([[ "${version1}" == "${version2}" ]] && echo 0 || echo 1)
+    case "${operator}" in
+        -eq) [[ "${same_versions}" -eq 0 ]] && return 0 || return 1;;
+        -ge|-le) [[ "${same_versions}" -eq 0 ]] && return 0;;
+        -ne) [[ "${same_versions}" -eq 0 ]] || return 0 && return 1;;
+        -gt|-lt) [[ "${same_versions}" -eq 0 ]] && return 1;;
+        *)
+            _siu::log::error "Bad parameter format: operator=\"${operator}\""
+            return 3
+            ;;
+    esac
+
+    for i in {1..3}; do
+        op1=$(echo "${version1}" | cut -d"." -f"${i}")
+        op2=$(echo "${version2}" | cut -d"." -f"${i}")
+        if eval "[[ ${op1} ${operator} ${op2} ]]"; then
+            return 0
+        fi
+    done
+
+    return 1
 }
